6bd29a279c4829bcb3446f109e206b231a97be4e
Scott Pinkelman <scott@scottpinkelman.com>
2017-02-10 11:28:54 -0700

    initial implmentation of ClimateZoneController with basic queries

---
+++ app/Http/Controllers/ClimateZoneController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use DB;
+
+
+class ClimateZoneController extends Controller
+{
+
+    public function climateZonesAll()
+    {
+
+            $zones = DB::select('select * from locations');
+
+            return response()->json($zones);
+
+    }
+
+    public function climateZonesCode($code)
+    {
+
+            $zones = DB::select('select * from locations where Cls = ?', [$code]);;
+
+            return response()->json($zones);
+
+    }
+
+    public function climateZonesLat($lat, $lon)
+    {
+
+            $zones = DB::select('select * from locations where Lat = :lat and Lon = :lon', [$lat, $lon]);;
+
+            return response()->json($zones);
+
+    }
+
+}
