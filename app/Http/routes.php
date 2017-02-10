3e99ab3a00030a193087fa20057f12d40594a316
Scott Pinkelman <scott@scottpinkelman.com>
2017-02-10 11:24:07 -0700

    adjusts indentation in routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -7,17 +7,17 @@
 */
 
 $app->get('/', function() use ($app) {
-    return "Welcome to the Climate Zone API.";
+    return 'Welcome to the Climate Zone API.';
 });
 
 $app->group(['prefix' => 'api/v1','namespace' => 'App\Http\Controllers'], function($app)
 {
 
-  $app->get('all','ClimateZoneController@climateZonesAll');
-  
-  $app->get('location/{lat}/{lon}','ClimateZoneController@climateZonesLat');
+    $app->get('all','ClimateZoneController@climateZonesAll');
 
-  $app->get('zone/{code}','ClimateZoneController@climateZonesCode');
+    $app->get('location/{lat}/{lon}','ClimateZoneController@climateZonesLat');
+
+    $app->get('zone/{code}','ClimateZoneController@climateZonesCode');
    
 });
 
