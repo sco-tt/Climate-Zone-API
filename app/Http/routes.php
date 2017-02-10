fe861b6fd2b12f5dc4220d58bff1e431fdb0ee6a
Scott Pinkelman <scott@scottpinkelman.com>
2017-02-10 09:42:31 -0700

    Initial commit

---
+++ app/Http/routes.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+*/
+
+$app->get('/', function() use ($app) {
+    return "Welcome to the Climate Zone API.";
+});
+
+$app->group(['prefix' => 'api/v1','namespace' => 'App\Http\Controllers'], function($app)
+{
+
+  $app->get('all','ClimateZoneController@climateZonesAll');
+  
+  $app->get('location/{lat}/{lon}','ClimateZoneController@climateZonesLat');
+
+  $app->get('zone/{code}','ClimateZoneController@climateZonesCode');
+   
+});
+
