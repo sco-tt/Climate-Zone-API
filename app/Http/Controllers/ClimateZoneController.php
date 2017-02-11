ef17d9123883c4002212a9385aa9b7eb0426d721
Scott Pinkelman <scott@scottpinkelman.com>
2017-02-11 09:34:23 -0700

    Runs PSR2 linting

--- app/Http/Controllers/ClimateZoneController.php
+++ app/Http/Controllers/ClimateZoneController.php
@@ -6,35 +6,29 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use DB;
 
-
 class ClimateZoneController extends Controller
 {
 
     public function climateZonesAll()
     {
+        $zones = DB::select('select * from locations');
 
-            $zones = DB::select('select * from locations');
-
-            return response()->json($zones);
-
+        return response()->json($zones);
     }
 
     public function climateZonesCode($code)
     {
+        $zones = DB::select('select * from locations where Cls = ?', [$code]);
+        ;
 
-            $zones = DB::select('select * from locations where Cls = ?', [$code]);;
-
-            return response()->json($zones);
-
+        return response()->json($zones);
     }
 
     public function climateZonesLat($lat, $lon)
     {
+        $zones = DB::select('select * from locations where Lat = :lat and Lon = :lon', [$lat, $lon]);
+        ;
 
-            $zones = DB::select('select * from locations where Lat = :lat and Lon = :lon', [$lat, $lon]);;
-
-            return response()->json($zones);
-
+        return response()->json($zones);
     }
-
 }
